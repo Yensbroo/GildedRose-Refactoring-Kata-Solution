0a7c5c576017b29bede6b6cc94fd9e179a48c35b
Yens Broothaers <yensbroothaers@gmail.com>
2024-08-20 09:14:16 +0200

    add tests for all items and normal items

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,9 +1,48 @@
-import { Item, GildedRose } from '@/gilded-rose';
+import { Item, GildedRose } from "@/gilded-rose";
+import { expect } from "vitest";
 
-describe('Gilded Rose', () => {
-  it('should foo', () => {
-    const gildedRose = new GildedRose([new Item('foo', 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe('fixme');
+const createItems = (items: Array<Item>) => {
+  const gildedRose = new GildedRose(items);
+
+  return gildedRose.updateQuality();
+};
+
+describe("Gilded Rose", () => {
+  describe("All items", () => {
+    it("should never have a negative quality", () => {
+      const item = new Item("Foo", 4, 0);
+      const items = createItems([item]);
+
+      expect(items[0].quality).toBe(0);
+    });
+
+    it("should never have a quality greater than 50, except for Sulfuras", () => {
+      const items = createItems([
+        new Item("Aged Brie", 4, 51),
+        new Item("Foo", 4, 52),
+        new Item("Backstage passes to a TAFKAL80ETC concert", 2, 51),
+      ]);
+
+      items.forEach((item) => {
+        console.info(`Quality of item ${item.name} is ${item.quality}`);
+        expect(item.quality).toBe(50);
+      });
+    });
+  });
+
+  describe("Normal items", () => {
+    it("should degrade quality by 1 when sellIn is bigger than 0", () => {
+      const item = new Item("Foo", 4, 20);
+      const items = createItems([item]);
+
+      expect(items[0].quality).toBe(19);
+    });
+
+    it("should degrade quality by 2 when sell by date has passed", () => {
+      const item = new Item("Foo", 0, 20);
+      const items = createItems([item]);
+
+      expect(items[0].quality).toBe(18);
+    });
   });
 });
